a7aafc451c697b674233bf624ded02e59846d5fd
Breana David <david.breana@student.usv.ro>
2026-03-17 21:37:49 +0200

    Tema_lab2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Masina m = new Masina();
+﻿Masina masina1 = new Masina("Mercedes C-Class", 2022, "benzina", 1999, 204, 15000, "automata", 35000, true);
+
+Client client1 = new Client("Popescu", "Ion", "1960101223344");
+
+Vanzare vanzare1 = new Vanzare(client1, masina1);
+
+Console.WriteLine(masina1.Model);
